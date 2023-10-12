e7fcf85928af5470d41a79c328cac2db8761f63f
Victor Paliari <victorrpaliari@gmail.com>
2023-10-12 16:28:17 -0300

    Spring Boot Structure

---
+++ todolist/src/test/java/br/com/victorpaliari/todolist/TodolistApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.victorpaliari.todolist;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TodolistApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
